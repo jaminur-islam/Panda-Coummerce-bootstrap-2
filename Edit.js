bf64752851909f00147a30053ee9e2b880477b63
Jaminur islam <jaminurislam250@gmail.com>
2021-08-14 19:05:07 +0600

    add new js file

--- Edit.js
+++ Edit.js
@@ -1,4 +1,4 @@
-/* // h1 color change 
+ // h1 color change 
 const h1 = document.getElementsByTagName('h1');
 for (const heading of h1){
   // console.log(heading.innerText);
@@ -8,9 +8,11 @@ for (const heading of h1){
 
 // card section er bgcolor change 
 const cardSection = document.getElementById('card-section');
+cardSection.addEventListener('dblclick' , function(){
+  cardSection.style.background  = 'pink';
+  cardSection.style.padding = '10px';
 
-cardSection.style.background  = 'pink';
-cardSection.style.padding = '10px';
+})
 
 
 
@@ -41,17 +43,46 @@ for(const oneBtn of btn){
     console.log(oneBtn.childNodes)
  oneBtn.addEventListener('click' , function(event){
   console.log(event.target.parentNode.removeChild(event.target));
-     
+  alert('BUY NOW')   
  })
-} */
+} 
 
 
 
-const btn = document.getElementsByTagName('button')[6];
-console.log(btn);
+// form desing js
+ const btn2 = document.getElementsByTagName('button')[6];
+console.log(btn2);
+
+btn2.addEventListener('click' , function(){
+  alert(input.value);
+  input.value = '';
+})
 
 const input = document.getElementById('input');
 
 input.addEventListener('keyup' , function(event){
-  console.log(event.target.value);
+ if (event.target.value == 'input'){
+   btn2.removeAttribute('disabled');
+ }else {
+   btn2.setAttribute('disabled' , true);
+ }
 })
+
+const subscribe = document.getElementById('subscribe');
+input.addEventListener('focus' , function(){
+  subscribe.style.backgroundColor = 'red';
+})
+input.addEventListener('blur' , function(){
+  subscribe.style.backgroundColor = 'white';
+}) 
+
+
+const img = document.getElementsByTagName('img');
+console.log(img);
+
+for(const im of img){
+  im.addEventListener('click' , function(){
+    im.src = 'bags/bag-2.png';
+  })
+
+}
